a5337c07428c3a736a81febcec50b10c8ffd2c25
Rizal <84571944+theonlyrizal@users.noreply.github.com>
2025-08-28 15:45:09 +0600

    done with JS

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,88 @@
 document.getElementById('body-main').addEventListener('click', function (event) {
   const clickedElement = event.target;
-
-  // Heart Click Mechanism
-  switch (clickedElement.id) {
-    case 'card-1-heart':
-    case 'card-2-heart':
-    case 'card-3-heart':
-    case 'card-4-heart':
-    case 'card-5-heart':
-    case 'card-6-heart':
-    case 'card-7-heart':
-    case 'card-8-heart':
-    case 'card-9-heart':
-      let heartCountElement = document.getElementById('heart-count');
-      let numberOfHeart = Number(heartCountElement.innerText);
-      numberOfHeart++;
-      heartCountElement.innerText = numberOfHeart;
-      break;
-    // Call Button Mechanism
-    case 'card-1-call-button':
-    case 'card-2-call-button':
-    case 'card-3-call-button':
-    case 'card-4-call-button':
-    case 'card-5-call-button':
-    case 'card-6-call-button':
-    case 'card-7-call-button':
-    case 'card-8-call-button':
-    case 'card-9-call-button':
-      let coinCountElement = document.getElementById('coin-count');
-      let numberOfCoin = Number(coinCountElement.innerText);
-      if (numberOfCoin < 20) {
-        alert(`⚠️ At lease 20 coins required to place a Call. Try again Later.`);
+  handler(clickedElement);
+  function handler(clickedElement) {
+    console.log(`handler called`);
+    // Heart Click Mechanism
+    switch (clickedElement.id) {
+      case 'card-1-heart':
+      case 'card-2-heart':
+      case 'card-3-heart':
+      case 'card-4-heart':
+      case 'card-5-heart':
+      case 'card-6-heart':
+      case 'card-7-heart':
+      case 'card-8-heart':
+      case 'card-9-heart':
+        let heartCountElement = document.getElementById('heart-count');
+        let numberOfHeart = Number(heartCountElement.innerText);
+        numberOfHeart++;
+        heartCountElement.innerText = numberOfHeart;
         break;
-      }
-      const callHistoryContainerElement = document.getElementById('call-history-container');
-      const buttonGrandParent = clickedElement.parentNode.parentNode;
-      const cardServiceName = document.getElementById(
-        buttonGrandParent.id + '-service-name'
-      ).innerText;
-      const cardServiceNumber = document.getElementById(
-        buttonGrandParent.id + '-service-number'
-      ).innerText;
-      numberOfCoin -= 20;
-      coinCountElement.innerText = numberOfCoin;
-      const childToAppend = createCallHistoryChild(cardServiceName, cardServiceNumber);
-      callHistoryContainerElement.appendChild(childToAppend);
-      alert(
-        `📞 Calling ${cardServiceName}, Number: ${cardServiceNumber} ... \n🪙 20 Coins will be deducted`
-      );
-      break;
-    case 'history-clear-button':
-      const elementToClear = document.getElementById('call-history-container');
-      elementToClear.innerHTML = '';
+      // Call Button Mechanism
+      case 'card-1-call-button':
+      case 'card-2-call-button':
+      case 'card-3-call-button':
+      case 'card-4-call-button':
+      case 'card-5-call-button':
+      case 'card-6-call-button':
+      case 'card-7-call-button':
+      case 'card-8-call-button':
+      case 'card-9-call-button':
+        let coinCountElement = document.getElementById('coin-count');
+        let numberOfCoin = Number(coinCountElement.innerText);
+        if (numberOfCoin < 20) {
+          alert(`⚠️ At lease 20 coins required to place a Call. Try again Later.`);
+          break;
+        }
+        const callHistoryContainerElement = document.getElementById('call-history-container');
+        const buttonGrandParent = clickedElement.parentNode.parentNode;
+        const cardServiceName = document.getElementById(
+          buttonGrandParent.id + '-service-name'
+        ).innerText;
+        const cardServiceNumber = document.getElementById(
+          buttonGrandParent.id + '-service-number'
+        ).innerText;
+        numberOfCoin -= 20;
+        coinCountElement.innerText = numberOfCoin;
+        const childToAppend = createCallHistoryChild(cardServiceName, cardServiceNumber);
+        callHistoryContainerElement.appendChild(childToAppend);
+        alert(
+          `📞 Calling ${cardServiceName}, Number: ${cardServiceNumber} ... \n🪙 20 Coins will be deducted`
+        );
+        break;
+      case 'history-clear-button':
+        const elementToClear = document.getElementById('call-history-container');
+        elementToClear.innerHTML = '';
+        break;
+      case 'card-1-copy-button':
+      case 'card-2-copy-button':
+      case 'card-3-copy-button':
+      case 'card-4-copy-button':
+      case 'card-5-copy-button':
+      case 'card-6-copy-button':
+      case 'card-7-copy-button':
+      case 'card-8-copy-button':
+      case 'card-9-copy-button':
+        const copyCountElement = document.getElementById('copy-count');
+        let copyCountNumber = Number(copyCountElement.innerText);
+        copyCountNumber++;
+        copyCountElement.innerText = copyCountNumber;
+        const copyGrandParent = clickedElement.parentNode.parentNode;
+        const copyServiceNumber = document.getElementById(
+          copyGrandParent.id + '-service-number'
+        ).innerText;
+        alert(`Copied number: ${copyServiceNumber}`);
+        navigator.clipboard.writeText(copyServiceNumber);
+        break;
+      default:
+        console.log(`default`);
+        const closestButton = clickedElement.closest('button');
+        if (closestButton) {
+          handler(closestButton);
+        }
+        break;
+    }
   }
 });
 
